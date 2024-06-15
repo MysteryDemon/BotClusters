fb6f8f94e3b7addbe960196b2b66bc27c57f91b5
Mystery Demon <161893464+MysteryDemon@users.noreply.github.com>
2024-06-15 01:59:58 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,18 @@
 for bot in $(cat config.json | jq -r 'to_entries[] | "\(.key),\(.value.source),\(.value.branch)"'); do
   name=$(echo $bot | cut -d',' -f1);
   source=$(echo $bot | cut -d',' -f2);
-  branch=$(echo $bot | cut -d',' -f3); 
+  branch=$(echo $bot | cut -d',' -f3);
 
   # Handle missing branch
   if [ -z "$branch" ]; then
-    branch="main" # Default to 'master' if branch is not specified
+    branch="master" 
   fi
 
-  git clone --branch $branch $source $name;
-  cd $name && pip install --no-cache-dir -r requirements.txt && cd ..;
+  # Fetch latest commit
+  git clone --branch $branch $source $name
+  cd $name
+  git fetch origin
+  git checkout $branch
+  pip install --no-cache-dir -r requirements.txt 
+  cd ..
 done
