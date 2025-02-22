6f2d8a50abf85d69145d411f02dbb5e3c06c81f0
Mystery Demon <161893464+MysteryDemon@users.noreply.github.com>
2025-02-22 15:10:22 -0800

    Create gunicorn.conf.py

---
+++ gunicorn.conf.py
@@ -0,0 +1,32 @@
+import multiprocessing
+
+# Server socket
+bind = '0.0.0.0:5000'
+backlog = 2048
+
+# Worker processes
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = 'sync'
+worker_connections = 1000
+timeout = 60
+keepalive = 2
+
+# Logging
+accesslog = '-'
+errorlog = '-'
+loglevel = 'info'
+
+# Process naming
+proc_name = 'gunicorn_flask'
+
+# Server mechanics
+daemon = False
+pidfile = None
+umask = 0
+user = None
+group = None
+tmp_upload_dir = None
+
+# SSL
+keyfile = None
+certfile = None
