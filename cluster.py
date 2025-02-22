7726c8b3bd1e1f5d1bbf48cba3b029b4c8882e00
Mystery Demon <161893464+MysteryDemon@users.noreply.github.com>
2025-02-22 14:40:51 -0800

    Update cluster.py

--- cluster.py
+++ cluster.py
@@ -1,8 +1,8 @@
 import subprocess
 import threading
 
-def run_app():
-    subprocess.run(["python3", "app.py"])
+def run_gunicorn():
+    subprocess.run(["gunicorn", "--workers=4", "--bind=0.0.0.0:8000", "--log-level=info", "--access-logfile=-", "--error-logfile=-", "run:app"])
 
 def run_ping_server():
     subprocess.run(["python3", "ping_server.py"])
@@ -13,18 +13,24 @@ def run_worker():
 def run_supervisord():
     subprocess.run(["/usr/bin/supervisord", "-n", "-c", "/etc/supervisor/supervisord.conf"])
 
+def run_update():
+    subprocess.run(["python3", "update.py"])
+
 if __name__ == "__main__":
     supervisord_thread = threading.Thread(target=run_supervisord)
-    app_thread = threading.Thread(target=run_app)
+    gunicorn_thread = threading.Thread(target=run_gunicorn)
     ping_server_thread = threading.Thread(target=run_ping_server)
     worker_thread = threading.Thread(target=run_worker)
+    update_thread = threading.Thread(target=run_update)
 
     supervisord_thread.start()
-    app_thread.start()
+    gunicorn_thread.start()
     ping_server_thread.start()
     worker_thread.start()
-
+    update_thread.start()
+    
     supervisord_thread.join()
-    app_thread.join()
+    gunicorn_thread.join()
     ping_server_thread.join()
     worker_thread.join()
+    update_thread.join()
