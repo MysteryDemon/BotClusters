0d2c1679dfa8a7804f278418ccd6e4be3c571c46
Mystery Demon <161893464+MysteryDemon@users.noreply.github.com>
2025-02-22 17:08:23 -0800

    Update gunicorn.conf.py\

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -8,7 +8,7 @@ bind = '0.0.0.0:5000'
 backlog = 2048
 
 # Worker processes
-workers = multiprocessing.cpu_count() * 2 + 1
+workers = 1
 worker_class = 'sync'
 worker_connections = 1000
 timeout = 60
