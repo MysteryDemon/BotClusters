dc26e665422f1edcaad61df6b53a46e944f75beb
Mystery Demon <161893464+MysteryDemon@users.noreply.github.com>
2025-02-22 18:40:52 -0800

    Update run.py

--- run.py
+++ run.py
@@ -8,8 +8,6 @@ SUPERVISOR_LOG_DIR = "/var/log/supervisor"
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-app = create_app()
-
 if __name__ == "__main__":
     try:
         os.makedirs(SUPERVISOR_LOG_DIR, exist_ok=True)
