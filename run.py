38fbfb831a2b09b8b5668b9846fe45fef137201a
Mystery Demon <161893464+MysteryDemon@users.noreply.github.com>
2025-02-22 16:23:07 -0800

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
-from app import app
+from app import app, socketio
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port, debug=True, use_reloader=False)
