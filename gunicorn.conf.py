73528a765f3cb36b5bff65e43e5ee11381533d8a
Mystery Demon <161893464+MysteryDemon@users.noreply.github.com>
2025-02-22 19:13:20 -0800

    Update gunicorn.conf.py

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,11 +1,14 @@
 import multiprocessing
 
+# Worker class
+worker_class = "eventlet"
+
 # Server socket
 bind = '0.0.0.0:5000'
 backlog = 2048
 
 # Worker processes
-workers = 1
+workers = multiprocessing.cpu_count() * 2 + 1 
 worker_class = 'sync'
 worker_connections = 1000
 timeout = 60
