feaf374b6b71550883b1517eff0be2c3b2f20ede
Mystery Demon <161893464+MysteryDemon@users.noreply.github.com>
2025-02-24 03:09:07 -0800

    Update run.py

--- run.py
+++ run.py
@@ -18,9 +18,8 @@ if __name__ == "__main__":
             app,
             host="0.0.0.0",
             port=port,
-            debug=False,    
-            use_reloader=False,
-            allow_unsafe_werkzeug=True
+            debug=True,    
+            use_reloader=False
         )
 
     except Exception as e:
