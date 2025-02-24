05a4552ac6fb39cff842cc5b1f06e8d7070bedc9
Mystery Demon <161893464+MysteryDemon@users.noreply.github.com>
2025-02-23 22:42:33 -0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,10 @@ import eventlet
 eventlet.monkey_patch()
 
 from flask import Flask
+from asgiref.wsgi import WsgiToAsgi
 
 app = Flask(__name__)
+
+asgi_app = WsgiToAsgi(app)
 
 from app import routes
