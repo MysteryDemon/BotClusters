beac7d6e82c02056a9a25319ff1fe6ee55e7abcd
Mystery Demon <161893464+MysteryDemon@users.noreply.github.com>
2025-04-19 21:28:15 -0700

    Update cluster.py

--- cluster.py
+++ cluster.py
@@ -10,6 +10,9 @@ def run_gunicorn():
     port = os.environ.get("PORT", 5000)
     subprocess.run(["gunicorn", "-w", "1", "-k", "eventlet", "-b", f"0.0.0.0:{os.environ.get('PORT', '5000')}", "run:app"], check=True)
 
+def run_supervisord():
+    subprocess.run(["/usr/bin/python3.10/supervisord", "-n", "-c", "/etc/supervisor/supervisord.conf"])
+
 def run_worker():
     subprocess.run(["python3", "worker.py"])
 
@@ -23,16 +26,16 @@ if __name__ == "__main__":
     time.sleep(2)
 
     gunicorn_thread = threading.Thread(target=run_gunicorn)
-    #supervisord_thread = threading.Thread(target=run_supervisord)
+    supervisord_thread = threading.Thread(target=run_supervisord)
     worker_thread = threading.Thread(target=run_worker)
     ping_server_thread = threading.Thread(target=run_ping_server)
 
     gunicorn_thread.start()
-    #supervisord_thread.start()
+    supervisord_thread.start()
     worker_thread.start()
     ping_server_thread.start()
 
     gunicorn_thread.join()
-    #supervisord_thread.join()
+    supervisord_thread.join()
     worker_thread.join()
     ping_server_thread.join()
