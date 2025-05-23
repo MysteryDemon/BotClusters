1ea0b57f4f666f6c822fdb92624259e8c06d8969
Mystery Demon <161893464+MysteryDemon@users.noreply.github.com>
2025-05-23 15:34:09 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 set -e
+
 usage() {
-    echo "Usage: $0 [--apt-packages \"pkg1 pkg2...\"] [--pip-packages \"pkg1 pkg2...\"]"
+    echo "Usage: $0 [--dnf-packages \"pkg1 pkg2...\"] [--pip-packages \"pkg1 pkg2...\"]"
     exit 1
 }
 
-APT_PACKAGES="ngnix"
+DNF_PACKAGES="nginx"
 PIP_PACKAGES="pytest gunicorn gallery-dl"
 
 while [[ $# -gt 0 ]]; do
     case $1 in
-        --apt-packages)
-            APT_PACKAGES="$2"
+        --dnf-packages)
+            DNF_PACKAGES="$2"
             shift 2
             ;;
         --pip-packages)
@@ -24,14 +25,17 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-if [ ! -z "$APT_PACKAGES" ]; then
-    echo "Installing apt packages: $APT_PACKAGES"
-    apt-get update
-    echo "$APT_PACKAGES" | xargs apt-get install -y
+if [ ! -z "$DNF_PACKAGES" ]; then
+    echo "Installing dnf packages: $DNF_PACKAGES"
+    dnf -y update
+    echo "$DNF_PACKAGES" | xargs dnf install -y
 fi
 
 if [ ! -z "$PIP_PACKAGES" ]; then
     echo "Installing pip packages: $PIP_PACKAGES"
-    which pip >/dev/null || apt-get install -y python3-pip
+    if ! command -v pip >/dev/null 2>&1; then
+        echo "pip not found, installing python3-pip..."
+        dnf install -y python3-pip
+    fi
     echo "$PIP_PACKAGES" | xargs pip install
 fi
