82c07158d7cce8a1a48850ab1da2f0d016758c8d
Mystery Demon <161893464+MysteryDemon@users.noreply.github.com>
2025-07-13 16:31:17 -0700

    Update run.py

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ eventlet.monkey_patch()
 import os
 import logging
 from app import app
-from app.routes.routes import socketio, schedule_cronjobs
+from app.routes.routes import socketio
 
 SUPERVISOR_LOG_DIR = "/var/log/supervisor"
 
@@ -13,7 +13,6 @@ logger = logging.getLogger(__name__)
 if __name__ == "__main__":
     try:
         os.makedirs(SUPERVISOR_LOG_DIR, exist_ok=True)
-        schedule_cronjobs()
         port = int(os.environ.get("PORT", 5000))
         socketio.run(
             app,
